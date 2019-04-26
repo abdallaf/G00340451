ebb527714f550441ba83289fd289ce9208f3d7f2
ABDALLA FADUL <G00340451@gmit.ie>
2019-04-26 22:40:34 +0100

    adding images

--- src/pages/submit/submit.ts
+++ src/pages/submit/submit.ts
@@ -3,9 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
  * Generated class for the SubmitPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
+ 
  */
 
 @IonicPage()
